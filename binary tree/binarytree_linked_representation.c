0b059b074b1a514198410501c188a6a9ef10e845
unknown <jainpiyushjain123@gmail.com>
2023-03-04 14:30:01 +0530

    height of the tree code is been added in binary tree

--- binary tree/binarytree_linked_representation.c
+++ binary tree/binarytree_linked_representation.c
@@ -1,12 +1,19 @@
+// in this program you will get inorder,postorder,preorder traversal and height of the binary tree
+// in this code we have use recursive functions
+
 #include<stdio.h>
 #include<conio.h>
 #include<malloc.h>
+#include<math.h>
+
+//structure has been defined
 struct node{
 	int data;
 	struct node* left;
 	struct node* right;
 };
 
+// function has been created to for creting a binary tree
 struct node* createNode(int data){
 	struct node *n;
 	n=(struct node *)malloc(sizeof(struct node));
@@ -20,25 +27,58 @@ struct node* createNode(int data){
 struct node * preorder(struct node *root){
 	if(root!=NULL){
 		printf(" %d",root->data);
-		printf("a");
+		//printf("a");
 		preorder(root->left);
-		printf("b");
+		//printf("b");
 		preorder(root->right);
-		printf("c");
+		//printf("c");
 		
 	}
 }
 
+// postorder traversal  left->right->root
+
 struct node* postorder(struct node *root){
 	if(root!=NULL){
-		printf("a");
+		//printf("a");
 		postorder(root->left);
-		printf("b");
+		//printf("b");
 		postorder(root->right);
-		printf("c");   
+		//printf("c");   
 		printf("%d ",root->data);
 	}
 }
+
+// Inorder traversal left->root->right
+struct node* inorder(struct node* root){
+	if(root!=NULL){
+		//printf("a");  
+		inorder(root->left);
+		//printf("b");
+		printf("%d ",root->data);
+		//printf("c");
+		inorder(root->right);
+		
+	}
+}
+
+// to get the height of the binary tree
+
+int getHeight(struct node* root) {
+   
+    if (!root)
+        return -1;
+    else {
+        
+        int left = getHeight(root->left);
+        int right = getHeight(root->right);
+        if (left >= right)
+            return left + 1;
+        else
+            return right + 1;
+    }
+}
+
 
 int main()
 {		//old way of assignning the values without funnction
@@ -66,17 +106,26 @@ int main()
 	struct node *p2=createNode(6);
 	struct node *p3=createNode(5);
 	struct node *p4=createNode(2);
-	
+	//struct node *p5=createNode(11);
+	//struct node *p6=createNode(3);
 	//linking the root node in the tree to its left and right nodes
 	p->left=p1;
 	p->right=p2;
 	p1->left=p3;
 	p1->right=p4;
+	//p3->left=p5;
+	//p3->right=p6;
 	printf("\nthe preorder is \n");
 	preorder(p);
 
-printf("\n\nthe postorder is \n");
-postorder(p);
+	printf("\n\nthe postorder is \n");
+	postorder(p);
+	printf("\n\n\n print inoreder \n");
+	inorder(p);
+
+	printf("\n\n\nheight of the tree : ");
+	printf("%d",getHeight(p));
+
 	
 	
 	return 0;
